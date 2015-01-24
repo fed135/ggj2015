cbb95074bded502477c16a72001663501766bf59
Frederic <fredericcharette@gmail.com>
2015-01-24 07:57:55 -0500

    fixed main start

--- client/js/game/main.js
+++ client/js/game/main.js
@@ -8,27 +8,29 @@ function gameStart(){
 			"Arstider/Dictionary",
 			"screens/Preloader",
 			"Arstider/GameData",
+			"Arstider/Viewport",
 
 			"media/fonts",
 			"media/gameConfig",
 			"media/strings",
 			"media/music/spriteInfo"
 		],
-		function(Browser, Engine, Sound, Fonts, Dictionary, preloaderContent, GameData, fonts, configs, strings, soundSprite){
+		function(Browser, Engine, Sound, Fonts, Dictionary, preloaderContent, GameData, Viewport, fonts, configs, strings, soundSprite){
 			
 			Dictionary.load(strings);
 			Fonts.load(fonts);
 			GameData.load(configs);
 			Sound.load("media/music/sprite", soundSprite);
 			
-			
-			Engine.start("main", true);
 			Engine.debug = true;
 			Arstider.verbose = 2;
+			Engine.start("main", true);
+			
 			Engine.setPreloaderScreen(preloaderContent);
 				
 			//#################################################
 			
+			Viewport._resize();
 			Engine.loadScreen("screens/title");
 		});
 	});
