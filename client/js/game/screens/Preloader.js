3eed41ffc70a47997b5c2740dc0013e288c6314e
Ariel Chouinard <ayoulc@hotmail.com>
2015-01-25 14:14:40 -0500

    laoding

--- client/js/game/screens/Preloader.js
+++ client/js/game/screens/Preloader.js
@@ -1,6 +1,6 @@
 define("screens/Preloader",[                         
-	"Arstider/DisplayObject","Arstider/Viewport"
-], function(DisplayObject, Viewport){
+	"Arstider/DisplayObject","Arstider/Viewport","Arstider/BitmapAnimation"
+], function(DisplayObject, Viewport, BitmapAnimation){
 	
 	return {
 		init:function()
@@ -14,22 +14,26 @@ define("screens/Preloader",[
 			});
 			this.addChild(this.bg);
 
-			this.bar = new DisplayObject({
+			this.bar = new BitmapAnimation({
 				name: "bar",
-				data: "media/images/screens/preloader/bar.png",
-				x: 280,
-				y: 318
+				x:500,
+				y:500,
+				spritesheet:"media/images/screens/preloader/loading"/*,
+            	speed:0.32*/
 			});
 			this.addChild(this.bar);
-			this.bar.dock(0.33,0.485);
 		},
+
 		update:function(pc)
 		{
-			var w = 576 * (pc * 0.01);
+
+			/*this.bar.currentFrame += 1;
+			if(this.bar.currentFrame > 3)this.bar.currentFrame = 0;*/
+			/*var w = 576 * (pc * 0.01);
 			
 			if(w < 1) w = 1;
 
-			this.bar.width = this.bar.dataWidth = w;
+			this.bar.width = this.bar.dataWidth = w;*/
 
 			// this.drawImage(bar, 0, 0, Math.floor(w), 27, 412, 347, Math.floor(w), 27);
 		}
